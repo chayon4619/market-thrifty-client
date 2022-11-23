c7f40a0c87c04d45339fc6585d31a97a9a9606fd
mrc4619 <mrchayon5525@gmail.com>
2022-11-23 21:18:09 +0600

    navbar and footer added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Toaster } from 'react-hot-toast';
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './Routes/Router';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="max-w-[1140px] mx-auto">
+      <RouterProvider router={router}></RouterProvider>
+      <Toaster></Toaster>
     </div>
   );
 }
